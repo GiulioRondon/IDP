d90dff5b9715f985707e10042524ee048b343f25
ThePatman3 <31772151+ThePatman3@users.noreply.github.com>
2018-01-31 12:19:55 +0100

    Updated Dijkstra.py
    Het algoritme ontwijkt nu ook obstakels.

--- Dijkstra.py
+++ Dijkstra.py
@@ -6,8 +6,10 @@
 # FindVertexFromCor(ListWithVertex, coordinate), Omdat er twee "soorten" coordinaten worden gebruikt in deze code,
 #   namelijk coordinaten in de form [xCor, yCor] en vertexen; coordinaten met extra informatie die belangrijk zijn voor
 #   het dijkstra algoritme, moest er een functie komen om de vertex te vinden bij een coordinaat.
-# Dijkstra(grid, source, destination), het algoritme! Deze berekend de snelste route van "source" naar "destination".
-#   De functie genereerd ook de benodigde vertexen.
+# Dijkstra(grid, source, destination), het algoritme! Deze berekend de snelste route van "source" naar "destination":
+#   De functie genereerd de benodigde vertexen.
+#   De functie ontwijkt obstakels
+# De functie berekend de snelste weg en geeft deze in een multidimensional list in de form: [[xCor, yCor], [xCor, yCor]]
 #####################
 
 import copy
@@ -17,27 +19,27 @@ gridCor = []                # De lijst waar alle coordinaten in worden gezet, in
 startCoordinate = []        # De lijst waar de start coordinaten in worden gezet, in de form [xCor, yCor]
 DestinationCoordinate = []  # De lijst waar de coordinaten van het doel in worden gezet, in de form [xCor, yCor]
 
-def GenCor(minX, maxX, minY, maxY):
+def genCor(minX, maxX, minY, maxY):
     maxX += 1
     maxY += 1
     lstCor = []
     for x in range(minX, maxX):              # Hier begint het genereren van alle coordinaten
         for y in range(minY, maxY):
             coordinate = [x, y]         # Een lijst met een coordinaat
-            lstCor.append(coordinate)  # De coordinaat wordt toegevoegd aan gridCor
+            lstCor.append(coordinate)  # De coordinaat wordt toegevoegd aan lstCor
     return lstCor
 
-gridCor = GenCor(1, 40, 1, 24)
+gridCor = genCor(1, 40, 1, 24)
 
 print(gridCor[-1])
 
 #Nu het generen van test obstakels
-# obstaclesCor = []
-# for x in range(10, 12):
-#     for y in range(1, 14):
-#         coordinateOb = [x, y]
-#         obstaclesCor.append(coordinateOb)
-# print("Obstacles = {} to {}".format(obstaclesCor[0], obstaclesCor[-1]))
+obstaclesCor = []
+for x in range(10, 12):
+    for y in range(1, 14):
+        coordinateOb = [x, y]
+        obstaclesCor.append(coordinateOb)
+print("Obstacles = {} to {}".format(obstaclesCor[0], obstaclesCor[-1]))
 
 # Daarna, omdat dit een test is, voert de gebruiker de start coordinaten in.
 startCoordinateX = int(input("Give x coordinate: "))
@@ -88,7 +90,7 @@ def FindVertexFromCor(ListWithVertex, coordinate):
     print("{} not found in list".format(coordinate))
 
 # Dit wordt de functie om de kortste route te berekenen.
-def Dijkstra(grid, source, destination):
+def Dijkstra(grid, source, destination, obstacles):
     """The function to calculate the shortest route with dijkstra's
     The parameter grid should be in the form [[xCor, yCor], [xCor yCor]]
     The parameter source should be in the form [xCor, yCor]"""
@@ -96,23 +98,25 @@ def Dijkstra(grid, source, destination):
     unvisited = copy.deepcopy(grid) # unvisited wordt een copy van het grid, wanneer een coordinaat is bezocht door het
                                     # algoritme wordt dit coordinaat uit unvisited gehaald.
     grid_Ext = copy.deepcopy(grid)  # Dit wordt het grid, maar met extra informatie belangrijk voor het algoritme,
-                        # een coordinaat/vertex staat hier in de form:
-                        # [xCor, Ycor, DistFromSource, OptimalPrevVertex, [neighbourVertex[xCor, yCor, dist]], visited]
+            # een coordinaat/vertex staat hier in de form:
+            # [xCor, Ycor, DistFromSource, OptimalPrevVertex, [neighbourVertex[xCor, yCor, dist]], visited, obstacle]
                         # Wanneer een coordinaat de extra informatie bevat, wordt het een vertex genoemd in de code
     sourceIndex = grid_Ext.index(source) # De index van het doel in grid_Ext.
     # Hieronder wordt de extra informatie toegevoegd aan elk coordinaat/vertex, waarbij:
-    # DistFromSource=-1, OptimalPrevVertex=[-1,-1], De lijst van buren is leeg, visited=0
+    # DistFromSource=-1, OptimalPrevVertex=[-1,-1], De lijst van buren is leeg, visited=0, obstacle=0
     for vertex in grid_Ext:
         vertex.append(10000000000)
         vertex.append([-1,-1])
         vertex.append([])
         vertex.append(0)
+        vertex.append(0)
         vertex[4] = neighboursCal(vertex, 40, 24)
-    # print(len(grid_Ext))
     # print(grid_Ext[3])
-    # print(grid_Ext[sourceIndex])
-    # print("<><><><>")
-    # print(unvisited[25])
+
+    # Zet waarde obstacle in alle obstacle vertexen op 1
+    for obstacle in obstacles:
+        obstacleVertex = FindVertexFromCor(grid_Ext, obstacle)
+        grid_Ext[grid_Ext.index(obstacleVertex)][6] = 1
 
     grid_Ext[sourceIndex][2] = 0 # zet de afstand tot de bron van de bron op 0
 
@@ -144,13 +148,12 @@ def Dijkstra(grid, source, destination):
                 grid_Ext[currentNeighbourIndex][3] = currenVertexCor# sneller en moet dit de nieuwe korste route worden
             listUnVisitedNeighbours.append(currentNeighbourCor) # De huidige buur moet wel bij listUnVisitedNeighbours
         try:
-            #print("Current vertex for remove = {}".format(currenVertexCor))
             unvisited.remove(currenVertexCor) # Wanneer de afstand tot alle buren berekend is, is deze coordinaat bezocht
         except ValueError:
             print("Value Error!\n{}\nStep = {}".format(currenVertexCor, step))
-            # print("Value? = {}".format(unvisited[25]))
-            # print(currenVertexCor)
-            # print("step = " + str(step))
+            print("Value? = {}".format(unvisited[25]))
+            print(currenVertexCor)
+            print("step = " + str(step))
             break
         grid_Ext[grid_Ext.index(currenVertex)][5] = 1 # Zet visited=1
 
@@ -158,40 +161,32 @@ def Dijkstra(grid, source, destination):
             print("Current step = " + str(step))
         if currenVertexCor == destination:
             print("Destination reached")
-            #print(currenVertexCor)
-            #print(destination)
             print("End step = " + str(step))
             break
-        #print("Wich cor is next??")
 
-        # Welke coordinaat moet nu worden bezocht? Dit wordt de coordinaat die het dichts bij de bron ligt en visited=0.
-        shortestDistanceToSource = 10000             # Om er voor te zorgen dat niet alle afstanden
+        # Welke coordinaat moet nu worden bezocht? Dit wordt de coordinaat die het dichts bij de bron ligt, visited=0,
+        shortestDistanceToSource = 10000             # En obstacle=0 heeft.Om er voor te zorgen dat niet alle afstanden
         for vertexxCor in listUnVisitedNeighbours:   # van alle coordinaten worden berekend, wordt het alleen berekend
             vertexx = FindVertexFromCor(grid_Ext, vertexxCor) # uit de lijst listUnVisitedNeighbours
-            #print("Vertexx: {}\nVisited? {}".format(vertexx, vertexx[5]))
-            if vertexx[5]== 1:
+            if vertexx[5]== 1 or vertexx[6] == 1: # Als die al bezocht is of een obstacle is...
                 listUnVisitedNeighbours.remove(vertexxCor)
                 continue
             if grid_Ext[grid_Ext.index(vertexx)][2] < shortestDistanceToSource:
                 shortestDistanceToSource = grid_Ext[grid_Ext.index(vertexx)][2]
                 currenVertexCor = vertexxCor
-                #print("New vertex for visiting is {}".format(currenVertexCor))
         step += 1
 
     # Nu moet de route berekend worden
-    #print("route calculation")
     route_step = 0
     route_currentVertexCor = copy.deepcopy(destination)
     route_routeCors = []
     while(True):
-        #print("Route calculationStep = {}, on vertex{}".format(route_step, route_currentVertexCor))
         route_routeCors.append(route_currentVertexCor)
         route_currentVertex = FindVertexFromCor(grid_Ext, route_currentVertexCor)
         route_currentVertexCor = route_currentVertex[3]
         if route_currentVertexCor == source:
             route_routeCors.append(route_currentVertexCor)
             break
-        #print("Next vertex in route = {}".format(route_currentVertexCor))
         route_step += 1
         if route_step > 1000:
             print("Infinate loop? Something went wrong!")
@@ -199,7 +194,7 @@ def Dijkstra(grid, source, destination):
     return route_routeCors
 
 
-theRoute = Dijkstra(gridCor, startCoordinate, DestinationCoordinate)
+theRoute = Dijkstra(gridCor, startCoordinate, DestinationCoordinate, obstaclesCor)
 
 print("+++++++++++++++++++++++++\n{}\n+++++++++++++++++++++++++".format(theRoute))
 
